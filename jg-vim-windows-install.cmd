eeec3e530d4083bc61160b371caf553eb963be8a
Joe G <joe.granados@hotmail.com>
2012-12-05 18:18:48 -0800

    initial commit

---
+++ jg-vim-windows-install.cmd
@@ -0,0 +1,13 @@
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
+
+@set BASE_DIR=%HOME%\.jg-vim
+call git clone --recursive -b 3.0 git://github.com/g-eoj/jg-vim.git %BASE_DIR%
+call mkdir %BASE_DIR%\.vim\bundle
+call mklink /J %HOME%\.vim %BASE_DIR%\.vim
+call mklink %HOME%\.vimrc %BASE_DIR%\.vimrc
+call mklink %HOME%\_vimrc %BASE_DIR%\.vimrc
+call mklink %HOME%\.vimrc.bundles %BASE_DIR%\.vimrc.bundles
+
+call git clone http://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
+call vim -u "$BASE_DIR/.vimrc.bundles" - +BundleInstall! +BundleClean +qall
