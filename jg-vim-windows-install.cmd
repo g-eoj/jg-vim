797bb3f90e771b9e2bd028d139f74fc6c8d78d0f
Joe G <joe.granados@hotmail.com>
2013-02-06 22:11:39 -0800

    comment out line in windows install cmd
    Line created folder that I don't think is needed.

--- jg-vim-windows-install.cmd
+++ jg-vim-windows-install.cmd
@@ -2,7 +2,7 @@
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 @set BASE_DIR=%HOME%\.jg-vim
-call mkdir %BASE_DIR%\.vim\bundle
+::call mkdir %BASE_DIR%\.vim\bundle
 call mklink /J %HOME%\.vim %BASE_DIR%\.vim
 call mklink %HOME%\_gvimrc %BASE_DIR%\.gvimrc
 call mklink %HOME%\_vimrc %BASE_DIR%\.vimrc
